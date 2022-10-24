ef10e0a1e232fe332937a63c44b6261995bedc40
Sangwook Lee <leesang627@gmail.com>
2022-10-24 15:55:33 +0900

    change into google-gtag

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -14,10 +14,14 @@ const config: GatsbyConfig = {
   graphqlTypegen: true,
   plugins: [
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingId: 'G-T46Q28G7H7',
+        trackingIds: [
+          'G-T46Q28G7H7', // 설정 Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
       },
     },
     'gatsby-plugin-image',
